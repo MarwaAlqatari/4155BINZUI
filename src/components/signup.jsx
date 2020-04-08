4146af176a2ab93e6acffaf24856b6c520170ca2
cking107 <cking107@uncc.edu>
2020-04-08 15:05:33 -0400

    Initial commit for branch - finish signup.jsx

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -2,6 +2,30 @@ import React, { Component } from "react";
 import { render } from "@testing-library/react";
 
 export default class SignUp extends Component {
+
+  constructor(props) {
+    super(props);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  onChangeEmail(e) {
+    this.setState({
+      email: e.target.value
+    });
+  }
+
+  onChangePassword(e) {
+    this.setState({
+      password: e.target.value
+    });
+  }
+
   render() {
     return (
       <div>
