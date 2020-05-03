ce4878915cf186c2c1ec1d5de98a9e2a8c2fa151
Cameron King <cking107@uncc.edu>
2020-05-03 16:08:12 -0400

    Finished frontend

--- src/components/signin.jsx
+++ src/components/signin.jsx
@@ -4,7 +4,7 @@ import { render } from "@testing-library/react";
 export default class SignIn extends Component {
   render() {
     return (
-      <div style={{ marginTop: 20 }}>
+      <div style={{ marginTop: 20 }, {marginLeft: 20}}>
         <h3>Sign In</h3>
         <form onSubmit={this.onSubmit}>
           <div className="form-group">
@@ -14,6 +14,7 @@ export default class SignIn extends Component {
               className="form-control"
               //value={this.state.email}
               //onChange={this.onChangeEmail}
+              style={{ width: "400px" }}
             />
           </div>
 
@@ -24,12 +25,13 @@ export default class SignIn extends Component {
               className="form-control"
               //value={this.state.password}
               //onChange={this.onChangePassword}
+              style={{ width: "400px" }}
             />
           </div>
 
           <div className="form-group">
           <button
-              //onClick={this.onNavigateUsers}
+              
               className="btn btn-primary"
             >
               Sign In
--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -7,11 +7,13 @@ export default class SignUp extends Component {
   constructor(props) {
     super(props);
     this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangeName = this.onChangeName.bind(this);
     this.onChangePassword = this.onChangePassword.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
 
     this.state = {
       email: "",
+      name: "",
       password: ""
     };
   }
@@ -22,20 +24,29 @@ export default class SignUp extends Component {
     });
   }
 
+  onChangeName(e) {
+    this.setState({
+      name: e.target.value
+    });
+  }
+
   onChangePassword(e) {
     this.setState({
       password: e.target.value
     });
   }
+  
 
   async onSubmit(e) {
     e.preventDefault();
     const formData = new FormData();
     formData.append("email", this.state.email);
+    formData.append("name", this.state.name);
     formData.append("password", this.state.password);
 
     console.log("Form submitted:");
     console.log(`Email: ${this.state.email}`);
+    console.log(`Name: ${this.state.name}`);
     console.log(`Password: ${this.state.password}`);
 
     const response = await axios.post(
@@ -58,7 +69,7 @@ export default class SignUp extends Component {
 
   render() {
     return (
-      <div style={{ marginTop: 20 }}>
+      <div style={{ marginTop: 20 }, {marginLeft: 20}}>
         <h3>Sign Up</h3>
         <form onSubmit={this.onSubmit}>
           <div className="form-group">
@@ -68,6 +79,18 @@ export default class SignUp extends Component {
               className="form-control"
               value={this.state.email}
               onChange={this.onChangeEmail}
+              style={{ width: "400px" }}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Name: </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.name}
+              onChange={this.onChangeName}
+              style={{ width: "400px" }}
             />
           </div>
 
@@ -78,12 +101,13 @@ export default class SignUp extends Component {
               className="form-control"
               value={this.state.password}
               onChange={this.onChangePassword}
+              style={{ width: "400px" }}
             />
           </div>
 
           <div className="form-group">
             <button
-              onClick={this.onNavigateUsers}
+              
               className="btn btn-primary"
             >
               Sign Up
